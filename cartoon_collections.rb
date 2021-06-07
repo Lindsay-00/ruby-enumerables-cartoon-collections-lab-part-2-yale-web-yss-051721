02fab05ef1ef6bebf9f140b12ae36a89155307e1
Lindsay Chen <lindsay.chen@yale.edu>
2021-06-07 02:51:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,9 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   planeteer_calls.each do |element|
     if element.length > 4
-      TRUE
+      return TRUE
     else
-      FALSE
+      return FALSE
     end
   end
 end
